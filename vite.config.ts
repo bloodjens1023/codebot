2044edb902bab861362a570002474edfcad54d53
bloodjens <hasiniainafanomezantsoa32@gmail.com>
2024-05-26 17:49:27 +0200

    dsds1234

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'ads.txt',
+          dest: ''
+        }
+      ]
+    })
+  ],
+  base: 'https://github.com/bloodjens1023/codebot.git' // Remplacez 'votre-depot' par le nom de votre dépôt GitHub
 })
