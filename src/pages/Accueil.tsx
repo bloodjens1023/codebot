6b321b9c4b5657764c3cc073bcc67d722c44bb55
bloodjens <hasiniainafanomezantsoa32@gmail.com>
2024-05-26 17:06:05 +0200

    dsds

---
+++ src/pages/Accueil.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Accueil = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Hello world {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+        style={{ marginLeft: "10px" }}
+      >
+        réinitialiser
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+        style={{ marginLeft: "10px" }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Accueil;
---
+++ src/pages/Jeux.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Jeux = () => {
+  return <div></div>;
+};
+
+export default Jeux;
